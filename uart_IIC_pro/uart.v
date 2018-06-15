02d9320da033853f1d16535252a4041260b9b216
fan Li <15201710458@163.com>
2018-06-15 16:51:54 +0800

    6.15 finish led&key pro and just start uart pro

---
+++ uart_IIC_pro/uart.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:16:31 06/15/2018 
+// Design Name: 
+// Module Name:    uart 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module uart(
+	input clk,
+	input rst_n,
+	input tx,
+	output rx
+    );
+
+
+endmodule
---
+++ uart_IIC_pro/uart_clk.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:37:12 06/15/2018 
+// Design Name: 
+// Module Name:    uart_clk 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module uart_clk(
+    input clk_50m,
+    output clk_out
+    );
+
+
+endmodule
